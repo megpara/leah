35cd88587d8f695dc7470d967cc77f91e213478c
Megan Paradowski <meganparadowski@Megans-MacBook-Pro.local>
2022-01-27 15:05:07 -0800

    Favicon fix

--- components/Layout.js
+++ components/Layout.js
@@ -6,7 +6,7 @@ export default function Layout({ children }) {
       <Head>
         <title>Cookies by Leah Lane</title>
         <meta name="description" content="Cookies by Leah Lane" />
-        <link rel="icon" type="image/png" href="/leah-favicon.png" />
+        <link rel="icon" type="image/png" href="/favicon2.png" />
         <link
           href="https://fonts.googleapis.com/css2?family=Forum&family=PT+Mono&family=Work+Sans:wght@100;200;300;400;500;600;700&display=swap"
           rel="stylesheet"
